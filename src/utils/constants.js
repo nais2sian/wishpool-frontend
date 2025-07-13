ce3e6270114fc6bd29541a820a27058f65c6ae47
NasZo <nais2sian@gmail.com>
2025-07-13 16:10:29 +0200

    updated back url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-export const URL = "http://localhost:3001";
+export const URL = "https://kupipodariday-backend.koyeb.app";
 export const MINIMUM_PASSWORD_LENGTH = 3;
 export const MINIMUM_USERNAME_LENGTH = 3;
 export const MAXIMUM_DESCRIPTION_LENGTH = 200;
